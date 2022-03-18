91b2b3eb46d0301914c397acdf842f4af377459c
carolyn-manning <carolyn.manning3@gmail.com>
2022-03-18 10:51:15 -0400

    create shell event listener for starting new game

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,9 @@
-console.log("hello")
+const startGameForm = document.getElementById('new-game-form')
+const startGameInput = document.getElementById('player-name-input')
+
+startGameForm.addEventListener("submit", startGame)
+
+function startGame() {
+    event.preventDefault()
+    console.log(startGameInput.value)
+}
