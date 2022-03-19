a0edc78f0e7cc8d0ac9860d5d1c65e4a7374308c
carolyn-manning <carolyn.manning3@gmail.com>
2022-03-18 20:30:54 -0400

    umcomment cors

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ const startGameForm = document.getElementById('new-game-form')
 const startGameInput = document.getElementById('player-name-input')
 const answerChoiceList = document.getElementById('choices')
 const questionText = document.getElementById('question-text')
+const scoreBoardContatiner = document.getElementById('score-board-container')
+const runningScoreContatiner = document.getElementById('running-score-container')
+const questionContainer = 
 // document.addEventListener('DOMContentLoaded', loadNewGameForm);
 
 // function loadNewGameForm() {
@@ -28,10 +31,28 @@ startGameForm.addEventListener("submit", startGame)
 
 function startGame() {
     event.preventDefault()
-    newGameContainer.remove()
     console.log(startGameInput.value)
-    answer = document.createElement('li')
+   
+    newGameContainer.remove()
+
+    let qs = ["question 1", "question 2", "question 3"]
+    
+    questionText.innerText = "Question 1"
+    const qText = document.getElementById('question-text')
+    let answer = document.createElement('li')
     answer.innerText = "Test Answer"
     answerChoiceList.appendChild(answer)
-    questionText.innerText = "Test Question"
+    
+    // add event listener with loop startGameForm.addEventListener("submit", startGame)
+
+    
+
+    let name = document.createElement('p')
+    let score = document.createElement('p')
+    score.innerText = "Score"
+    name.innerText = startGameInput.value
+
+    runningScoreContatiner.append(name, score)
+    
+    
 }
