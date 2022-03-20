ba71ceaf67e8bf093513c92e7d838dd2eef7b5ca
carolyn-manning <carolyn.manning3@gmail.com>
2022-03-20 01:00:17 -0400

    refactor

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,35 +10,17 @@ const usersURL = 'http://localhost:3000/users'
 const questionsURL = 'http://localhost:3000/questions'
 const gamesURL = 'http://localhost:3000/games'
 
-
-// document.addEventListener('DOMContentLoaded', loadNewGameForm);
-
-// function loadNewGameForm() {
-//     const h3 = document.createElement('h3')
-//     h3.innerText = 'Welcome to the Trivia App. Enter you name below to start a new Game.'
-//     const form = document.createElement('form')
-//     form.id = 'new-game-form'
-//     const input = document.createElement('input')
-//     input.type = 'text'
-//     input.id = 'player-name-input'
-//     const submit = document.createElement('input') 
-//     submit.type = "submit"
-//     submit.id = 'player-name-submit'
-//     submit.value = "Let's Get Started!"
-       
-//     newGameContainer.appendChild(h3)
-//     newGameContainer.appendChild(form)
-//     form.appendChild(input)
-//     form.appendChild(submit)
-// }
-
 startGameForm.addEventListener("submit", startGame)
 
 function startGame() {
     event.preventDefault()
+    setTimeout(endGame, 5000)
+    createUserAndGame()
+    newGameContainer.remove()
+    fetchQuestions()
+}
 
-   setTimeout(endGame, 5000)
-
+function createUserAndGame() {
     const configObj = {
         method: "POST", 
         headers: {
@@ -52,67 +34,56 @@ function startGame() {
     
     fetch(usersURL, configObj)
     .then(response => response.json())
-    .then(data => {
-        let title = document.createElement('h3')
+    .then(data => buildRunningScore(data));
+}
+
+function buildRunningScore(data) {
+    let title = document.createElement('h3')
         let score = document.createElement('p')
         score.id = "running-score"
         score.dataset.id = data["games"].slice(-1)[0]['id']
         score.innerText = "0"
         title.innerText = "Your Score"
         runningScoreContatiner.append(title, score)
-    });
-       
-    newGameContainer.remove()
-    fetchQuestions()
-    
-    // questionText.innerText = "Question 1"
-    // const qText = document.getElementById('question-text')
-    // let answer = document.createElement('li')
-    // answer.innerText = "Test Answer"
-    // answerChoiceList.appendChild(answer)
-    
-    // add event listener with loop startGameForm.addEventListener("submit", startGame)
 }
 
 function fetchQuestions() {
     fetch(questionsURL)
     .then(response => response.json())
-    .then(data => {
-        for (let i = 0; i < data.length; i++){
-            const question = document.createElement('h3')
-            question.innerText = data[i]["question"]
-            question.id = `q-${data[i]["id"]}`
-            let answerContainer = document.createElement('ul')
-            answerContainer.id = `answers-${data[i]["id"]}`
-            question.appendChild(answerContainer)
-            questionContainer.appendChild(question)
+    .then(data => displayQuestions(data))
+}
 
-            answerArray = [data[i]["choice_1"], data[i]["choice_2"], data[i]["choice_3"], data[i]["answer"]]
+function displayQuestions(data) {
+    for (let i = 0; i < data.length; i++){
+        const question = document.createElement('h3')
+        question.innerText = data[i]["question"]
+        question.id = `q-${data[i]["id"]}`
+        const answerContainer = document.createElement('ul')
+        //answerContainer.id = `answers-${data[i]["id"]}`
+        question.appendChild(answerContainer)
+        questionContainer.appendChild(question)
 
-            for (let n = 0; n < answerArray.length; n++) {
-                let answer = document.createElement('li')
-                answer.innerText = answerArray[n]
-                answerContainer.appendChild(answer)
-            }
-            answerContainer.addEventListener("click", (e) => {
-                answerQuestion(e, data[i]['answer'])
-            })
+        answerArray = [data[i]["choice_1"], data[i]["choice_2"], data[i]["choice_3"], data[i]["answer"]]
 
+        for (let n = 0; n < answerArray.length; n++) {
+            let answer = document.createElement('li')
+            answer.innerText = answerArray[n]
+            answerContainer.appendChild(answer)
         }
-    })
-    
+        answerContainer.addEventListener("click", (e) => {
+            answerQuestion(e, data[i]['answer'])
+        })
+    }
 }
 
 function answerQuestion(e, answer) {
+    const questionContainer = e.target.parentElement
+    question = questionContainer.parentElement
     if(e.target.innerText === answer) {
-        const questionContainer = e.target.parentElement
-        question = questionContainer.parentElement
         question.innerText = "CORRECT"
         question.style.color = "green"
         OptUpdateScore()
     } else {
-        const questionContainer = e.target.parentElement
-        question = questionContainer.parentElement
         question.innerText = "INCORRECT"
         question.style.color = "red"
     }
@@ -128,7 +99,7 @@ function OptUpdateScore() {
 function endGame() {
     updateScoreInDB()
     questionContainer.remove()
-    fetchScore()
+    fetchScoreboard()
 }
 
 function updateScoreInDB() {
@@ -143,22 +114,24 @@ function updateScoreInDB() {
             score: parseInt(scoreHTML.innerText)
         })
     }
-    
     fetch(`http://localhost:3000/games/${parseInt(scoreHTML.dataset.id)}`, configObj)
 }
 
-function fetchScore() {
+function fetchScoreboard() {
     fetch(gamesURL)
     .then(response => response.json())
-    .then(data => {
-        data.sort((a, b) => (a.score < b.score) ? 1 : -1)
+    .then(data => buildScoreboard(data))
+}
+
+function buildScoreboard(data) {
+    data.sort((a, b) => (a.score < b.score) ? 1 : -1)
         scoreboard = document.createElement("table")
         for (let i = 0; i < 25; i++){
             const row = document.createElement("tr")
             const name = document.createElement("td")
             const score = document.createElement("td")
             score.innerText = `${data[i]['score']}`
-            
+
             if(data[i]['user']['name'] === ''){
                 name.innerText = "Unknown Player"
             } else {
@@ -169,8 +142,6 @@ function fetchScore() {
             scoreboard.appendChild(row)
         }
         scoreBoardContatiner.appendChild(scoreboard)
-
-    })
 }
 
 
